e1cec0f56cbc6cae8be2d2a72067ac24900044de
S.-L. Xie <82627490+xiesl97@users.noreply.github.com>
2026-01-08 19:36:02 +0800

    fix-categories-loading

--- js/data-config.js
+++ js/data-config.js
@@ -12,7 +12,7 @@ const DATA_CONFIG = {
      * GitHub repository owner (username)
      * This will be replaced during GitHub Actions workflow execution
      */
-    repoOwner: 'dw-dengwei',
+    repoOwner: 'xiesl97',
 
     /**
      * GitHub repository name
